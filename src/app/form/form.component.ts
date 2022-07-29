8361b2fc1ba8b3e2ea5d351c0bf090d97b68f6e1
Saba Mchedlishvili <sabamchedlishvili17@gmail.com>
2022-07-29 17:23:16 +0400

    add User Tables

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import {passwordValidator} from "../password-validator";
+import {of} from "rxjs";
 
 
 @Component({
@@ -9,27 +10,29 @@ import {passwordValidator} from "../password-validator";
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-  forms = new FormGroup<any>('');
+  // forms: FormGroup<any> = new FormGroup('');
+  forms = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern("[A-Za-z0-9]+"), Validators.minLength(7)]),
+    confirmPassword: new FormControl('',[Validators.required, Validators.pattern("[A-Za-z0-9]+"),Validators.minLength(7)]),
+    nickname: new FormControl('',[Validators.required,
+      Validators.pattern('^[a-zA-Z0-9-]*$')
+    ]),
+    phone: new FormControl('', [Validators.required, Validators.pattern("^(\\+995\\s?)?((\\([0-9]{3}\\))|[0-9]{3})[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}$")]),
+    website: new FormControl('', [Validators.required,
+      Validators.pattern("(https?:\\/\\/)?(www\\.)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)|(https?:\\/\\/)?(www\\.)?(?!ww)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
+    ]),
+    checkbox: new FormControl('', [ Validators.requiredTrue])
+  }, {validators: passwordValidator});
+  users: any = [];
   constructor() { }
 
   ngOnInit(): void {
-    this.forms = new FormGroup({
-      email: new FormControl('',[Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.pattern("[A-Za-z0-9]+")]),
-      confirmPassword: new FormControl('',[Validators.required, Validators.pattern("[A-Za-z0-9]+"),Validators.minLength(7)]),
-      nickname: new FormControl('',[Validators.required,
-        Validators.pattern('^[a-zA-Z0-9-]*$')
-      ]),
-      phone: new FormControl('', [Validators.required, Validators.pattern("^(\\+995\\s?)?((\\([0-9]{3}\\))|[0-9]{3})[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}$")]),
-      website: new FormControl('', [Validators.required,
-        Validators.pattern("(https?:\\/\\/)?(www\\.)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)|(https?:\\/\\/)?(www\\.)?(?!ww)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
-      ]),
-      checkbox: new FormControl('', [ Validators.requiredTrue])
-    }, {validators: passwordValidator})
   }
+
 
   public onClick(): void {
-    console.log(this.forms);
+    this.users.push(this.forms.value);
+    this.forms.reset();
   }
-
 }
